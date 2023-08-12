14688db0ef820deb8c39e7d0ca0bb53301609619
宇羽田杨 <XYCSJ2019@outlook.com>
2023-08-12 11:16:42 +0800

    更改动画出入场，更改背景图片分辨率

--- miHaYo Launcher/MainWindow.xaml.cs
+++ miHaYo Launcher/MainWindow.xaml.cs
@@ -21,17 +21,140 @@ namespace miHaYo_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string opening = "0";
+        private bool main_background_show = false;
+
         public MainWindow()
         {
             this.ResizeMode = ResizeMode.CanMinimize;
             InitializeComponent();
             var iconsb = (Storyboard)this.FindResource("Stratup");
             iconsb.Begin();
+            main_background_show = true;
         }
 
         private void button_b2_Click(object sender, RoutedEventArgs e)
         {
-            
+            imageout();
+            if (opening == "b2")
+            { return; }
+            else
+            {
+                opening = "b2";
+                Storyboard sb = (Storyboard)this.FindResource("b2_in");
+                sb.Begin();
+            }
+        }
+
+        private void button_b3_Click(object sender, RoutedEventArgs e)
+        {
+            imageout();
+            if (opening == "b3")
+            { return; }
+            else
+            {
+                opening = "b3";
+                Storyboard sb = (Storyboard)this.FindResource("b3_in");
+                sb.Begin();
+            }
+
+        }
+
+        private void button_sr_Click(object sender, RoutedEventArgs e)
+        {
+            imageout();
+            if (opening == "sr")
+            { return; }
+            else
+            {
+                opening = "sr";
+                Storyboard sb = (Storyboard)this.FindResource("sr_in");
+                sb.Begin();
+            }
+
+        }
+
+        private void button_wd_Click(object sender, RoutedEventArgs e)
+        {
+            imageout();
+            if (opening == "wd")
+            { return; }
+            else
+            {
+                opening = "wd";
+                Storyboard sb = (Storyboard)this.FindResource("wd_in");
+                sb.Begin();
+            }
+
+        }
+
+        private void button_ys_Click(object sender, RoutedEventArgs e)
+        {
+            imageout();
+            if (opening == "ys")
+            { return; }
+            else
+            {
+                opening = "ys";
+                Storyboard sb = (Storyboard)this.FindResource("ys_in");
+                sb.Begin();
+            }
+
+        }
+
+        private void button_jq0_Click(object sender, RoutedEventArgs e)
+        {
+            imageout();
+            if (opening == "jp0")
+            { return; }
+            else
+            {
+                opening = "jq0";
+                Storyboard sb = (Storyboard)this.FindResource("jq0_in");
+                sb.Begin();
+            }
+
+        }
+
+        private void imageout()
+        {
+            if (main_background_show == true)
+            {
+                Storyboard sb = (Storyboard)this.FindResource("mbg_out");
+                sb.Begin();
+                main_background_show = false;
+            }
+
+            if (opening == "b2")
+            {
+                Storyboard sb = (Storyboard)this.FindResource("b2_out");
+                sb.Begin();
+            }
+            else if (opening == "b3")
+            {
+                Storyboard sb = (Storyboard)this.FindResource("b3_out");
+                sb.Begin();
+            }
+            else if (opening == "sr")
+            {
+                Storyboard sb = (Storyboard)this.FindResource("sr_out");
+                sb.Begin();
+            }
+            else if (opening == "wd")
+            {
+                Storyboard sb = (Storyboard)this.FindResource("wd_out");
+                sb.Begin();
+            }
+            else if (opening == "ys")
+            {
+                Storyboard sb = (Storyboard)this.FindResource("ys_out");
+                sb.Begin();
+            }
+            else if (opening == "jq0")
+            {
+                Storyboard sb = (Storyboard)this.FindResource("jq0_out");
+                sb.Begin();
+            }
         }
     }
 }
