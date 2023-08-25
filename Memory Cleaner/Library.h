aebdf242d359a394cd516edde85cee68b5c33fa9
宇羽田杨 <XYCSJ2019@outlook.com>
2023-08-25 21:34:24 +0800

    Revert "在解决方案中添加 Memory Cleaner 项目"
    This reverts commit c58371e2faca80afb8d5cfbbfb6798a6e193eb08.

--- Memory Cleaner/Library.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pch.h"
-#define SE_INCREASE_QUOTA_NAME  TEXT("SeIncreaseQuotaPrivilege")
-
-void deepClean();
-BOOL EmptyAllSet();
-bool AdjustTokenPrivilegesForNT();
-bool EnableSpecificPrivilege(LPCTSTR lpPrivilegeName);
