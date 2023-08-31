dbabdedb9bc3f67cfba49b8f360416357f931173
宇羽田杨 <XYCSJ2019@outlook.com>
2023-08-31 16:38:21 +0800

    更新版本号使其有意义； 更新 memory cleaner.dll 版本 1.0.0.0->1.0.0.29400

--- miHaYo Launcher/AssemblyInfo.cs
+++ miHaYo Launcher/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +10,11 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: AssemblyCopyright("Copyright © YuYuTianYang 2023")]//版权
+[assembly: AssemblyCompany("YuYuTianYang")]//公司
+[assembly: AssemblyTitle("miHaYo Launcher")]//文件说明
+[assembly: AssemblyProduct("MHYL")]//产品名
+[assembly: AssemblyVersion("0.13.2.*")]//版本号 [主号[次号[修订号[00:00:00-现在 的秒数/2]]]]
+[assembly: AssemblyConfiguration("free")]//版本附加信息
+[assembly: ComVisible(false)]//COM端口可访问性
